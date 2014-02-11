f8f09532d5a7c11beec5fe5865636285069f3967
ladislav.seps <ladislav.seps@tul.cz>
2014-02-11 13:23:39 +0100

    svn conversion prace s pedaly presunuta do samostatneho exe souboru, ktery bude muset byt vydan pod GPL licenci

---
+++ UpdateBuilder/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml.Linq;
+using System.Security.Cryptography;
+using Ionic.Zip;
+
+namespace UpdateBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            args = new string[]{@"C:\Users\Ttxman\Documents\NanoTrans\Work\WpfApplication2\bin\x86\Debug","Filelist.lst","300",@"http://admin.notebook.ttxman.operaunite.com/file_sharing_1/content/"};
+            Console.WriteLine("Creating definitions");
+            string basedir = args[0];
+            string list = args[1];
+            string build = args[2];
+            string datastore = args[3];
+            string packageDir = Path.Combine("packages",build);
+            if (Directory.Exists(packageDir))
+            {
+                Directory.GetFiles(packageDir, "*.*", SearchOption.AllDirectories).ToList().ForEach(f => File.Delete(f));
+                Directory.Delete(packageDir);
+            }
+
+            Directory.CreateDirectory(packageDir);
+
+            string[] files = File.ReadAllLines(list).Select(l => Path.Combine(basedir, l)).ToArray();
+            SHA1Cng sha = new SHA1Cng();
+
+            var document =  new XDocument(
+                                new XDeclaration("1.0","utf-8","true"),
+                                new XElement("UpdateDefinitions",
+                                    new XElement("Definition", new XAttribute("Application","NanoTrans"),new XAttribute("Build",build),new XAttribute("DataStoreURL",datastore),
+                                        files.Select(f=>new XElement("File",new XAttribute("FileName",f.Substring(basedir.Length+1)),new XAttribute("SHA1",Convert.ToBase64String(sha.ComputeHash(File.OpenRead(f))))))
+                                    )
+                                )
+                            );
+
+            Console.WriteLine("saving definitions");
+            document.Save(Path.Combine(packageDir,"Definitions.xml"));
+
+            Console.WriteLine("Zipping files");
+            foreach(string f in files)
+            {
+                Console.WriteLine("Zipping "+f);
+                ZipFile zf = new ZipFile();
+                zf.CompressionLevel = Ionic.Zlib.CompressionLevel.BestCompression;
+                zf.AddFile(f);
+                string fullpath = Path.Combine(packageDir, f.Substring(basedir.Length + 1)) + ".zip";
+                Directory.CreateDirectory(Path.GetDirectoryName(fullpath));
+                zf.Save(Path.Combine(packageDir, f.Substring(basedir.Length + 1)) + ".zip");
+                zf.Dispose();
+            }
+            Console.WriteLine("all done");
+        }
+    }
+}
--- Updater/App.xaml.cs
+++ Updater/App.xaml.cs
@@ -6,7 +6,10 @@ using System.Linq;
 using System.Windows;
 using System.IO;
 using System.Diagnostics;
-
+using System.Xml.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using Ionic.Zip;
 namespace Updater
 {
     /// <summary>
@@ -14,25 +17,74 @@ namespace Updater
     /// </summary>
     public partial class App : Application
     {
-        static string ExeName= null;
+        static string ExeName = null;
         static string ExeDir = null;
         static string NTExe = null;
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            ExeName = Environment.GetCommandLineArgs()[0];
-            ExeDir = Path.GetDirectoryName(ExeName);
-            NTExe = ExeDir + "\\NanoTrans.exe";
+            try
+            {
+                ExeName = Environment.GetCommandLineArgs()[0];
+                ExeDir = Path.GetDirectoryName(ExeName);
+                NTExe = ExeDir + "\\NanoTrans.exe";
 
-            if (File.Exists(NTExe))
-            {
-                FileVersionInfo fv = FileVersionInfo.GetVersionInfo(NTExe);
+                int version = 0;
+                if (File.Exists(NTExe))
+                {
+                    FileVersionInfo fv = FileVersionInfo.GetVersionInfo(NTExe);
+                    version = fv.FilePrivatePart;
+                }
+                string URL = ConfigurationManager.AppSettings["UpdateDefinitions"];
+
+                WebClient client = new WebClient();
+
+                var document = XDocument.Load(new MemoryStream(client.DownloadData(URL)));
+                var definition = document.Descendants("Definition").OrderByDescending(d => int.Parse(d.Attribute("Build").Value)).First();
+
+                if (int.Parse(definition.Attribute("Build").Value) > version)
+                {
+                    var filelist = definition.Descendants("File").ToList();
+
+                    List<XElement> flist = new List<XElement>();
+                    SHA1Cng sha = new SHA1Cng();
+                    foreach (var f in filelist)
+                    {
+                        string file = ExeDir + "\\" + f.Attribute("FileName").Value;
+                        if (File.Exists(file))
+                        {
+                            string h = Convert.ToBase64String(sha.ComputeHash(File.OpenRead(file)));
+                            string h2 = f.Attribute("SHA1").Value;
+                            if (h != h2)
+                            {
+                                flist.Add(f);
+                            }
+                        }
+                        else
+                        {
+                            flist.Add(f);
+                        }
+                    }
+
+
+                    foreach (var f in flist)
+                    {
+                        
+                        string path = f.Attribute("FileName").Value;
+                        string targetf = Path.Combine(ExeDir,path);
+                        Directory.CreateDirectory(Path.GetDirectoryName(targetf));
+                        string sourcef = definition.Attribute("DataStoreURL").Value +"/"+ path.Replace('\\','/')+".zip";
+                        var zf = ZipFile.Read(new MemoryStream(client.DownloadData(sourcef)));
+                        zf.Entries.First().Extract(File.OpenWrite(targetf));
+                        
+                    }
+                }
+
             }
-            else
+            catch
             {
-                MessageBox.Show("Nanotrans nenalezen ukončuji updater", "Nanotrans nenalezen", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("Chyba při načítání definic updatu, ukončuji updater", "Chyba", MessageBoxButton.OK, MessageBoxImage.Error);
                 Application.Current.Shutdown();
             }
-
         }
     }
 }
