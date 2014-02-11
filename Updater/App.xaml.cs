d9db0a295f02dc61f8345c504742bc27826ab582
ladislav.seps <ladislav.seps@tul.cz>
2014-02-11 13:23:39 +0100

    svn conversion prvni verze updateru ... uz to umi generovat filelisty a stahovat.. chybi poradny UI pro stahovani...

---
+++ Updater/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.IO;
+using System.Diagnostics;
+
+namespace Updater
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static string ExeName= null;
+        static string ExeDir = null;
+        static string NTExe = null;
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            ExeName = Environment.GetCommandLineArgs()[0];
+            ExeDir = Path.GetDirectoryName(ExeName);
+            NTExe = ExeDir + "\\NanoTrans.exe";
+
+            if (File.Exists(NTExe))
+            {
+                FileVersionInfo fv = FileVersionInfo.GetVersionInfo(NTExe);
+            }
+            else
+            {
+                MessageBox.Show("Nanotrans nenalezen ukončuji updater", "Nanotrans nenalezen", MessageBoxButton.OK, MessageBoxImage.Error);
+                Application.Current.Shutdown();
+            }
+
+        }
+    }
+}
