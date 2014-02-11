1ab23a894ca2ce0df6dbb49927ae905fe2d3fc73
ladislav.seps <ladislav.seps@tul.cz>
2014-02-11 13:23:04 +0100

    svn conversion opravy bugu : nacitani mluvcich nezalezi an poradi tagu, preskakovani kurzoru jinam pri vyberu pozice kliknutim ve vlne, odstavce zacinajici v -1 se nactou jako kdyby zacinali na konci minuleho odstavce.
    lepsi chybove hlasky u nacitani trsx souboru (zobrazuje se navic radek a znak)

--- WpfApplication2/Properties/AssemblyInfo.cs
+++ WpfApplication2/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.010927.28")]
-[assembly: AssemblyFileVersion("1.0.110927.32")]
+[assembly: AssemblyVersion("1.0.011002.12")]
+[assembly: AssemblyFileVersion("1.0.111002.12")]
