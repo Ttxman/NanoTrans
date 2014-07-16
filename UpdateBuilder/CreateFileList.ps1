5b140a298352ef7e84b294cf0447d92436ca803d
ladislav.seps <ladislav.seps@tul.cz>
2014-07-16 14:59:14 +0200

    removed no longer used subprojects -  svn versioning , and old updaters

--- UpdateBuilder/CreateFileList.ps1
+++
@@ -1,8 +0,0 @@
-$fullpath = Resolve-Path "..\WpfApplication2\bin\x86\Release\"
-$files = Get-ChildItem $fullpath -recurse | 
-where {!$_.PSIsContainer}
-
-if(Test-Path "filelist.lst"){ del "filelist.lst"}
-
-$files | 
-foreach {$_.FullName.Substring($fullpath.Path.Length) >> "filelist.lst" }
