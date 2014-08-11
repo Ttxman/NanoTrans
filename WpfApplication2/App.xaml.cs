fdd2e43b86c5104e1a41eca6c0d064aca2cc4127
ladislav.seps <ladislav.seps@tul.cz>
2014-08-11 16:33:58 +0200

    removed logging to file in debug .. this will be eventually replaced by some form of better log that is usable with release version

--- WpfApplication2/App.xaml.cs
+++ WpfApplication2/App.xaml.cs
@@ -23,11 +23,7 @@ namespace NanoTrans
 
         void CurrentDomain_FirstChanceException(object sender, System.Runtime.ExceptionServices.FirstChanceExceptionEventArgs e)
         {
-#if DEBUG
-            if(!File.Exists("nanotrans.log"))
-                File.Create("nanotrans.log").Dispose();
-            File.AppendAllText("nanotrans.log", string.Format("{6}: FirstChanceException event raised in {0}: {1}\r\nsource: {2}\r\n trace = {3}\r\nex:{4}\r\ninner:{5}\r\r\n\r\r\n\r\n\r\n", AppDomain.CurrentDomain.FriendlyName, e.Exception.Message, e.Exception.Source, e.Exception.StackTrace,e.Exception,e.Exception.InnerException, DateTime.Now));
-#endif
+
         }
 
     }
