Ttxman/NanoTrans

commit 6fe97876c569b90aac5dc6c9cc21358dd4621723

Xml libraries in python (and probably some other languages) do not automatically differentiate between null, empty string (and not specified field on dynamic object?). I had to process some .trsx files with empty dbid attribute on speaker, generated from python.
To prevent further problems, from now on: empty string value of dbid is handled same null value.
trsx XSD was modified to forbid empty string in dbid attribute

Transcription/Speaker.cs
@@ -487,6 +487,7 @@ namespace NanoTrans.Core
         /// if DataBaseType is DBType.User - modification is disabled
         /// if DataBaseType is DBType.File - processing of value is disabled
         /// SHOULD be always UNIQUE GUID-like string (NanoTrans expects that ids from DBType.API and DBType.User can't conflict)
+        /// empty string is automatically converted to null, dbid will be generated on next read
         /// </summary>
         public string DBID
         {
@@ -502,7 +503,7 @@ namespace NanoTrans.Core
             set
             {
                 if (string.IsNullOrWhiteSpace(_dbid))
-                    _dbid = value;
+                    _dbid = null;
                 else if (DataBaseType == DBType.User)
                     throw new ArgumentException("cannot change DBID when Dabase is User");
                 else

TrsxV1Plugin/LoadRes.cs
@@ -221,10 +221,9 @@ namespace TrsxV1Plugin
 
 
     }
+
     public static class LoadRes
     {
-
-
 
         public static bool Import(Stream input, Transcription storage)
         {

WpfApplication2/TRSXSchema3.xsd
@@ -93,7 +93,13 @@
       </xs:element>
     </xs:sequence>
     <xs:attribute name="id" type="xs:int" use="required"/>
-    <xs:attribute name="dbid" type="xs:string" use="optional"/>
+    <xs:attribute name="dbid" use="optional">
+      <xs:simpleType>
+        <xs:restriction base="xs:string">
+          <xs:minLength value="1"/>
+        </xs:restriction>
+      </xs:simpleType>
+    </xs:attribute>
     <xs:attribute name="dbtype" use="optional">
       <xs:simpleType >
         <xs:restriction base="xs:string">

commit 9567434bd8bfd374778f5b2912d36078b4c8fb72

fix: commit 6fe97876c569b90aac5dc6c9cc21358dd4621723 broke loading dbid in Speaker.

Transcription/Speaker.cs
@@ -502,8 +502,10 @@ namespace NanoTrans.Core
             }
             set
             {
-                if (string.IsNullOrWhiteSpace(_dbid))
+                if (string.IsNullOrWhiteSpace(value))
                     _dbid = null;
+                else if (string.IsNullOrWhiteSpace(_dbid))
+                    _dbid = value;
                 else if (DataBaseType == DBType.User)
                     throw new ArgumentException("cannot change DBID when Dabase is User");
                 else