169f228e2393289ab14e1de0636ee4b54e9b244e
ladislav.seps <ladislav.seps@tul.cz>
2014-09-29 13:46:12 +0200

    new fake plugin for importing audio with empty transcription

---
+++ FakeTrsxPlugin/FakeTrsxPlugin.cs
@@ -0,0 +1,24 @@
+﻿using NanoTrans.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeTrsxPlugin
+{
+    public class FakeTrsxPlugin
+    {
+        public static bool Import(Stream input, Transcription storage)
+        {
+            if (input is FileStream)
+            { 
+                storage.Add(new TranscriptionParagraph());
+                storage.MediaURI = ((FileStream)input).Name;
+                return true;
+            }
+            return false;
+        }
+    }
+}
